8a9f9bf4b836940e4afce37c0c79f202186b4999
monalikamble1001 <monalikamble1001@gmail.com>
2021-08-04 14:19:02 +0530

    [monika] created demo3

---
+++ src/Sample1/demo3.java
@@ -0,0 +1,14 @@
+package Sample1;
+
+public class demo3 {
+public static void main(String[] args) {
+	System.out.println("how are you");
+	System.out.println("how are you");
+	System.out.println("how are you");
+	System.out.println("how are you");
+	System.out.println("how are you");
+	System.out.println("how are you");
+	System.out.println("how are you");
+	System.out.println("how are you");
+}
+}
